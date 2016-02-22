d03be32188595e85abe1a2c8e4c1436995d64f9d
alanl <alan.junk.filter@gmail.com>
2016-02-22 22:21:54 +0000

    Added a local unit test with coverage enabled. Proved 100% line coverage of FCMLineParser but not all true/false tests evaluated. Had to create the local unit test manually after changing Build Variants to the 'Unit Tests' Test Artifact. I did this by using copy and paste of the Android instrumented test, FCMLineParserTest. I pasted it as FCMLineParserTestLocal into a directory structure matching the source file but under src/test rather than src/main. There must be an easier way!

---
+++ app/src/test/java/com/mycompany/myfcmapp/FCMLineParserTestLocal.java
@@ -0,0 +1,62 @@
+package com.mycompany.myfcmapp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FCMLineParserTestLocal {
+
+    @Test
+    public void testReset() throws Exception
+    {
+        FCMLineParser parser = new FCMLineParser();
+
+        // Use a subscriber line to initialise the parser.
+        String testLine = "S 10.174.8.147:53074";
+        parser.Parse(testLine);
+        Assert.assertEquals('S', parser.GetLineType());
+        Assert.assertEquals("10.174.8.147", parser.GetIPAddress());
+
+        // Test the Reset method.
+        parser.Reset();
+        Assert.assertEquals(' ', parser.GetLineType());
+        Assert.assertEquals("0.0.0.0", parser.GetIPAddress());
+    }
+
+    @Test
+    public void testParse() throws Exception
+    {
+        FCMLineParser parser = new FCMLineParser();
+
+        // Test subscriber line.
+        String testLine = "S 10.174.8.147:53074";
+        parser.Parse(testLine);
+        Assert.assertEquals('S', parser.GetLineType());
+        Assert.assertEquals("10.174.8.147", parser.GetIPAddress());
+
+        // Test publisher line.
+        testLine = "P 10.30.66.45:34053";
+        parser.Parse(testLine);
+        Assert.assertEquals('P', parser.GetLineType());
+        Assert.assertEquals("10.30.66.45", parser.GetIPAddress());
+
+        // Test case where this is not a publisher line.
+        testLine = "BP <LZPROD> /lz/sentinel_position/realtime/cashflow ...";
+        parser.Parse(testLine);
+        Assert.assertEquals(' ', parser.GetLineType());
+        Assert.assertEquals("0.0.0.0", parser.GetIPAddress());
+    }
+
+    @Test
+    public void testGetLineType() throws Exception
+    {
+        // Tests covered in the testParse method.
+    }
+
+    @Test
+    public void testGetIPAddress() throws Exception
+    {
+        // Tests covered in the testParse method.
+    }
+}
